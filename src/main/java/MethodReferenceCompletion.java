72e236f58c0ef4aca333f413446e84c5ef4b27f7
Nikita Eshkeev <kastolom@gmail.com>
2019-12-20 20:07:39 +0300

    WIP: additional entries in the completion dialog
    Signed-off-by: Nikita Eshkeev <kastolom@gmail.com>

--- src/main/java/MethodReferenceCompletion.java
+++ src/main/java/MethodReferenceCompletion.java
@@ -1,14 +1,26 @@
 import com.intellij.codeInsight.completion.*;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.lang.java.JavaLanguage;
 import com.intellij.patterns.PlatformPatterns;
+import com.intellij.patterns.PsiElementPattern;
+import com.intellij.patterns.PsiExpressionPattern;
+import com.intellij.psi.JavaTokenType;
+import com.intellij.psi.PsiIdentifier;
+import com.intellij.psi.PsiMethodReferenceExpression;
+import com.intellij.psi.PsiParenthesizedExpression;
+import com.intellij.psi.impl.source.tree.JavaElementType;
+import com.intellij.psi.impl.source.tree.java.PsiIdentifierImpl;
+import com.intellij.psi.impl.source.tree.java.PsiMethodReferenceExpressionImpl;
 import com.intellij.util.ProcessingContext;
 import org.jetbrains.annotations.NotNull;
 
 public class MethodReferenceCompletion extends CompletionContributor  {
+
     public MethodReferenceCompletion() {
+
         extend(
                 CompletionType.BASIC,
-                PlatformPatterns.psiElement(),
+                PlatformPatterns.psiElement(PsiIdentifier.class).withParent(PsiMethodReferenceExpression.class),
                 new MethodReferenceCompletionProvider()
         );
     }
@@ -21,7 +33,17 @@ public class MethodReferenceCompletion extends CompletionContributor  {
                 @NotNull final ProcessingContext context,
                 @NotNull final CompletionResultSet result
         ) {
-            result.addElement(LookupElementBuilder.create("NEW_COMPLETION").bold());
+//            System.out.println(parameters.);
+            result.addElement(LookupElementBuilder.create("NEW_COMPLETION").bold().withInsertHandler(
+                    (ctx, item) -> {
+                        System.out.println(item + " is selected");
+                    }
+            ));
         }
     }
+
+    private static CompletionContributor create() {
+        return null;
+    }
+
 }
