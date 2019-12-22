9e0b14f1f54e57db4afe0b2d705948be3793ad3f
Nikita Eshkeev <kastolom@gmail.com>
2019-12-22 18:23:42 +0300

    WIP: Add a class cast to Function
    This patch adds the "compose" entry to the completion dialog to method
    references that can be casted to java.util.function.Function. After the
    entry is selected the method reference gets casted to
    java.util.function.Function with deduced type parameters.
    
    Signed-off-by: Nikita Eshkeev <kastolom@gmail.com>

--- src/main/java/MethodReferenceCompletionContributor.java
+++ src/main/java/MethodReferenceCompletionContributor.java
@@ -1,11 +1,20 @@
 import com.intellij.codeInsight.completion.*;
+import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
 import com.intellij.patterns.PsiElementPattern;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiMethodReferenceExpression;
-import com.intellij.psi.PsiReferenceParameterList;
+import com.intellij.psi.*;
+import com.intellij.psi.impl.source.PsiClassImpl;
+import com.intellij.psi.impl.source.PsiMethodImpl;
+import com.intellij.psi.impl.source.tree.java.PsiMethodReferenceExpressionImpl;
+import com.intellij.psi.impl.source.tree.java.PsiParenthesizedExpressionImpl;
 import com.intellij.util.ProcessingContext;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 import static com.intellij.patterns.PlatformPatterns.psiElement;
 
@@ -27,6 +36,7 @@ public class MethodReferenceCompletionContributor extends CompletionContributor 
         final PsiElementPattern.Capture<PsiElement> classLocation = psiElement().beforeLeaf(psiElement().withText("::"));
         final PsiElementPattern.Capture<PsiElement> firstParenthesisLocation = psiElement().beforeLeafSkipping(psiElement().withText("::"), psiElement().beforeLeaf("("));
 
+        // FIXME: this element pattern handles false positive cases like "System." or when there is no receiver
         final PsiElementPattern.Capture<PsiElement> elementPattern = methodLocation
                 .andNot(firstParenthesisLocation)
                 .andNot(classLocation)
@@ -49,13 +59,108 @@ public class MethodReferenceCompletionContributor extends CompletionContributor 
                 @NotNull final ProcessingContext context,
                 @NotNull final CompletionResultSet result
         ) {
-            result.addElement(LookupElementBuilder.create("NEW_COMPLETION").bold().withInsertHandler(
-                    (ctx, item) -> {
-                        final PsiElement forElement = ctx.getFile().findElementAt(ctx.getSelectionEndOffset() - item.getLookupString().length() - 2);
-                        System.out.println(item.getLookupString() + " is selected for " + forElement.getNode());
-                    }
-            ));
+            final PsiElement element = parameters.getOriginalFile().findElementAt(parameters.getOffset() - 5);
+            if (element == null) return;
+
+            // FIXME: this check should be gone when the element pattern is fixed
+            final PsiElement parent = element.getParent();
+            if (!(parent instanceof PsiMethodReferenceExpressionImpl)) return;
+
+            final PsiMethodReferenceExpressionImpl methodReference = (PsiMethodReferenceExpressionImpl) parent;
+            final Stream<PsiMethod> methods;
+
+            if (methodReference.isConstructor()) {
+                methods = methodsFromConstructors(methodReference);
+            } else {
+                final PsiMethodImpl resolve = (PsiMethodImpl) methodReference.resolve();
+                methods = Stream.of(resolve);
+            }
+
+            final Predicate<PsiMethod> singleArgument = m -> m.getParameterList().getParametersCount() == 1;
+            final Predicate<PsiMethod> voidReturn = m -> "void".equals(m.getReturnType().getCanonicalText());
+
+            final Predicate<PsiMethod> isConstructor = m -> methodReference.isConstructor();
+
+            final Predicate<PsiMethod> singleArgumentConstructor = isConstructor.and(singleArgument);
+            final Predicate<PsiMethod> singleArgumentMethod = singleArgument.and(voidReturn.negate());
+
+            final Consumer<PsiMethod> addCompose = m -> {
+                final PsiTypeElement[] typeParams = methodReference.getParameterList().getTypeParameterElements();
+                final String typeFrom;
+
+                if (typeParams.length != 0) {
+                    typeFrom = typeParams[0].getText();
+                } else {
+                    final PsiType type = m.getParameterList().getParameters()[0].getType();
+                    typeFrom = fromPrimitive(type);
+                }
+                final String typeTo;
+                if (m.isConstructor()) {
+                    typeTo = m.getContainingClass().getName();
+                } else {
+                    final PsiType returnType = m.getReturnType();
+
+                    typeTo = fromPrimitive(returnType);
+                }
+                final InsertHandler<LookupElement> handler =
+                        (ctx, item) -> {
+                            final PsiElementFactory factory = JavaPsiFacade.getInstance(ctx.getProject()).getElementFactory();
+
+                            final String text = "((Function<" + typeFrom + ", " + typeTo + ">)" +
+                                    methodReference.getText() +
+                                    ")";
+
+                            final PsiParenthesizedExpressionImpl castExpr =
+                                    (PsiParenthesizedExpressionImpl)
+                                            factory.createExpressionFromText(
+                                                    text,
+                                                    null
+                                            );
+                            methodReference.replace(castExpr);
+                        };
+
+                result.addElement(LookupElementBuilder.create("compose").withTailText("()").withInsertHandler(handler).bold());
+            };
+
+            methods.filter(singleArgumentConstructor.or(singleArgumentMethod))
+                    .forEach(addCompose);
+        }
+
+        @NotNull
+        private String fromPrimitive(@NotNull final PsiType type) {
+            final String typeTo;
+            if (type instanceof PsiPrimitiveType) {
+                typeTo = ((PsiPrimitiveType) type).getBoxedTypeName();
+            } else {
+                typeTo = type.getCanonicalText(true);
+            }
+            return typeTo;
+        }
+
+        @Contract(pure = true)
+        @NotNull
+        private Stream<PsiMethod> methodsFromConstructors(@NotNull final PsiMethodReferenceExpressionImpl methodReference) {
+            final Stream<PsiMethod> methods;
+            final PsiElement constructor = methodReference.resolve();
+            if (constructor == null) {
+                // multiple constructors
+                final JavaResolveResult[] javaResolveResults = methodReference.multiResolve(true);
+                methods = Arrays.stream(javaResolveResults)
+                        .map(JavaResolveResult.class::cast)
+                        .map(JavaResolveResult::getElement)
+                        .map(PsiMethod.class::cast)
+                ;
+            } else if (constructor instanceof PsiMethod) {
+                // single constructor defined
+                methods =  Stream.of((PsiMethod) constructor);
+            } else if (constructor instanceof PsiClassImpl) {
+                // no constructor defined
+                final PsiClassImpl resolve = (PsiClassImpl) methodReference.resolve();
+                methods = resolve == null ? Stream.empty() : Arrays.stream(resolve.getConstructors());
+            } else {
+                throw new IllegalArgumentException("I don't know how to handle constructor " + constructor);
+            }
+            return methods;
         }
     }
-
 }
