fc808cd45fa9cfcc10bf5c544c04d9602c26f742
Nikita Eshkeev <kastolom@gmail.com>
2019-12-23 22:14:24 +0300

    WIP: Add new entries to completion dialog
    This patch adds a new version of MethodReferenceCompletionProvider that
    uses FunctionalInterfaceSuggester#suggestFunctionalInterfaces which
    returns the list of functionalinterfaces that a method reference can be
    casted to. The list of functionalinterfaces is provided into the
    completion dialog.
    
    Known issues:
    
    1. The element pattern does not work, because when the completion dialog
    is invoked the AST is in the broken state and there is no rigig method
    refenence structure, which makes impossible to provide an obvious
    pattern. A possible solution: provide my own parser and lexer in order
    to maintain the keep the valid PSI;
    2. FunctionalInterfaceSuggester provides just a list of functional
    interfaces without substituting the information about Generic types. The
    straightforward solution is too primitive, intellij has to provide a
    means to easily substitute these generic types;
    3. The actual cast in does not occur in the editor yet;
    4. The code is ugly.
    
    Signed-off-by: Nikita Eshkeev <kastolom@gmail.com>

--- src/main/java/MethodReferenceCompletionContributor.java
+++ src/main/java/MethodReferenceCompletionContributor.java
@@ -1,27 +1,69 @@
 import com.intellij.codeInsight.completion.*;
 import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.patterns.PatternCondition;
 import com.intellij.patterns.PsiElementPattern;
 import com.intellij.psi.*;
 import com.intellij.psi.impl.source.PsiClassImpl;
+import com.intellij.psi.impl.source.PsiClassReferenceType;
 import com.intellij.psi.impl.source.PsiMethodImpl;
 import com.intellij.psi.impl.source.tree.java.PsiMethodReferenceExpressionImpl;
-import com.intellij.psi.impl.source.tree.java.PsiParenthesizedExpressionImpl;
+import com.intellij.psi.util.PsiUtil;
 import com.intellij.util.ProcessingContext;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Objects;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static com.intellij.codeInsight.FunctionalInterfaceSuggester.suggestFunctionalInterfaces;
 import static com.intellij.patterns.PlatformPatterns.psiElement;
 
 public class MethodReferenceCompletionContributor extends CompletionContributor {
 
     public MethodReferenceCompletionContributor() {
 
+        final PsiElementPattern.Capture<PsiElement> elementPattern = getElementPattern();
+
+        extend(
+                CompletionType.SMART,
+                elementPattern
+                ,new MethodReferenceCompletionProvider()
+        );
+    }
+
+    @NotNull
+    private PsiElementPattern.Capture<PsiElement> getElementPattern1() {
+        Function<Object, String> s = Objects::toString;
+        return psiElement()
+                .with(new PatternCondition<PsiElement>("methodReference") {
+                    @Override
+                    public boolean accepts(@NotNull PsiElement psiElement, ProcessingContext context) {
+                        final PsiElement paramsList = psiElement.getPrevSibling();
+                        final PsiElement doubleColumn = paramsList.getPrevSibling();
+                        final PsiElement referenceExpression = doubleColumn.getPrevSibling();
+                        final PsiElement methodReference = psiElement.getParent();
+
+                        return psiElement instanceof PsiIdentifier
+                                && paramsList instanceof PsiReferenceParameterList
+                                && (doubleColumn instanceof PsiJavaToken && doubleColumn.getText().equals("::"))
+                                && referenceExpression instanceof PsiReferenceExpression
+                                && methodReference instanceof PsiMethodReferenceExpression
+                                ;
+                    }
+                })
+                ;
+    }
+
+    @NotNull
+    private PsiElementPattern.Capture<PsiElement> getElementPattern() {
         final PsiElementPattern.Capture<PsiElement> methodLocation = psiElement()
                 .afterLeafSkipping(
                         psiElement()
@@ -37,21 +79,95 @@ public class MethodReferenceCompletionContributor extends CompletionContributor 
         final PsiElementPattern.Capture<PsiElement> firstParenthesisLocation = psiElement().beforeLeafSkipping(psiElement().withText("::"), psiElement().beforeLeaf("("));
 
         // FIXME: this element pattern handles false positive cases like "System." or when there is no receiver
-        final PsiElementPattern.Capture<PsiElement> elementPattern = methodLocation
+        return methodLocation
                 .andNot(firstParenthesisLocation)
                 .andNot(classLocation)
                 .andNot(doubleColumnLocationAfter)
                 .andNot(parameterListLocationAfter)
-                .andNot(parameterListLocationBefore)
-                ;
-        extend(
-                CompletionType.BASIC,
-                elementPattern
-                ,new MethodReferenceCompletionProvider()
-        );
+                .andNot(parameterListLocationBefore);
     }
 
     private static final class MethodReferenceCompletionProvider extends CompletionProvider<CompletionParameters> {
+
+        @Override
+        protected void addCompletions(
+                @NotNull final CompletionParameters parameters,
+                @NotNull final ProcessingContext context,
+                @NotNull final CompletionResultSet result
+        ) {
+            System.out.println("HELLO");
+            if (true) return;
+            final PsiElement element = parameters.getOriginalFile().findElementAt(parameters.getOffset() - 5);
+            if (element == null) return;
+
+            // FIXME: this check should be gone when the element pattern is fixed
+            final PsiElement parent = element.getParent();
+            if (!(parent instanceof PsiMethodReferenceExpressionImpl)) return;
+
+            final PsiMethodReferenceExpressionImpl methodReference = (PsiMethodReferenceExpressionImpl) parent;
+
+            final Collection<? extends PsiType> types;
+            if (methodReference.isConstructor()) {
+                types = suggestFunctionalInterfaces(methodReference);
+            } else {
+                final PsiMethodImpl resolve = (PsiMethodImpl) methodReference.resolve();
+                assert resolve != null;
+                types = suggestFunctionalInterfaces(resolve);
+            }
+            System.out.println(types);
+
+            final Map<PsiClass, ? extends PsiType> classesToTypes = types.stream()
+                    .collect(
+                            Collectors.toMap(
+                                    PsiUtil::resolveClassInType,
+                                    Function.identity()
+                            )
+                    );
+
+            classesToTypes.keySet()
+                    .stream()
+                    .flatMap(e -> Arrays.stream(e.getMethods()))
+                    .forEach(m -> {
+                        result.addElement(
+                                LookupElementBuilder.create(m.getName())
+                                        .withTailText(m.getParameterList().getText())
+                                        .withTypeText(m.getContainingClass().getName())
+                                        .withPsiElement(m)
+                                        .bold()
+                                        .withInsertHandler((@NotNull InsertionContext ctx, @NotNull LookupElement item) -> {
+
+                                            final PsiMethod psiElement = (PsiMethod) item.getPsiElement();
+                                            assert psiElement != null;
+
+                                            final PsiType casting = classesToTypes.get(psiElement.getContainingClass());
+
+                                            if (casting instanceof PsiClassType) {
+                                                final PsiClassReferenceType aClass = (PsiClassReferenceType) ((PsiClassType) casting).getParameters()[0];
+//                                                System.out.println(aClass);
+//                                                System.out.println(aClass.resolveGenerics().getElement().getText());
+//                                                ((PsiClassType) casting).getParameters()
+                                            }
+
+                                            final PsiElementFactory factory = JavaPsiFacade.getInstance(ctx.getProject()).getElementFactory();
+
+                                            final PsiTypeCastExpression cast = (PsiTypeCastExpression) factory.createExpressionFromText(
+                                                    "(" + casting.getPresentableText() + ") ", null
+                                            );
+                                            System.out.println(cast.getCastType());
+
+                                            final PsiParenthesizedExpression parens = (PsiParenthesizedExpression) factory.createExpressionFromText(
+                                                    "(" + cast.getText() + methodReference.getText() + ")", null
+                                            );
+//                                            methodReference.replace(parens);
+                                        })
+                        );
+                    });
+            ;
+
+        }
+    }
+
+    private static final class MethodReferenceCompletionProvider1 extends CompletionProvider<CompletionParameters> {
 
         @Override
         protected void addCompletions(
@@ -85,6 +201,9 @@ public class MethodReferenceCompletionContributor extends CompletionContributor 
             final Predicate<PsiMethod> singleArgumentMethod = singleArgument.and(voidReturn.negate());
 
             final Consumer<PsiMethod> addCompose = m -> {
+                final Collection<? extends PsiType> psiTypes = suggestFunctionalInterfaces(m);
+                psiTypes.stream().map(PsiType::getCanonicalText).forEach(System.err::println);
+
                 final PsiTypeElement[] typeParams = methodReference.getParameterList().getTypeParameterElements();
                 final String typeFrom;
 
@@ -105,24 +224,31 @@ public class MethodReferenceCompletionContributor extends CompletionContributor 
                 final InsertHandler<LookupElement> handler =
                         (ctx, item) -> {
                             final PsiElementFactory factory = JavaPsiFacade.getInstance(ctx.getProject()).getElementFactory();
+                            final PsiTypeCastExpression cast = (PsiTypeCastExpression) factory.createExpressionFromText("(Function<" + typeFrom + ", " + typeTo + ">) ", null);
 
-                            final String text = "((Function<" + typeFrom + ", " + typeTo + ">)" +
-                                    methodReference.getText() +
-                                    ")";
+                            final PsiParenthesizedExpression parens = (PsiParenthesizedExpression) factory.createExpressionFromText(
+                                    "(" + cast.getText() + methodReference.getText() + ")", null
+                            );
 
-                            final PsiParenthesizedExpressionImpl castExpr =
-                                    (PsiParenthesizedExpressionImpl)
-                                            factory.createExpressionFromText(
-                                                    text,
-                                                    null
-                                            );
-                            methodReference.replace(castExpr);
+//                            final String text = "((Function<" + typeFrom + ", " + typeTo + ">)" +
+//                                    methodReference.getText() +
+//                                    ")";
+//
+//                            final PsiParenthesizedExpressionImpl castExpr =
+//                                    (PsiParenthesizedExpressionImpl)
+//                                            factory.createExpressionFromText(
+//                                                    text,
+//                                                    null
+//                                            );
+//                            methodReference.replace(castExpr);
+                            methodReference.replace(parens);
                         };
 
                 result.addElement(LookupElementBuilder.create("compose").withTailText("()").withInsertHandler(handler).bold());
             };
 
             methods.filter(singleArgumentConstructor.or(singleArgumentMethod))
+                    .peek(System.out::println)
                     .forEach(addCompose);
         }
 
@@ -136,31 +262,31 @@ public class MethodReferenceCompletionContributor extends CompletionContributor 
             }
             return typeTo;
         }
+    }
 
-        @Contract(pure = true)
-        @NotNull
-        private Stream<PsiMethod> methodsFromConstructors(@NotNull final PsiMethodReferenceExpressionImpl methodReference) {
-            final Stream<PsiMethod> methods;
-            final PsiElement constructor = methodReference.resolve();
-            if (constructor == null) {
-                // multiple constructors
-                final JavaResolveResult[] javaResolveResults = methodReference.multiResolve(true);
-                methods = Arrays.stream(javaResolveResults)
-                        .map(JavaResolveResult.class::cast)
-                        .map(JavaResolveResult::getElement)
-                        .map(PsiMethod.class::cast)
-                ;
-            } else if (constructor instanceof PsiMethod) {
-                // single constructor defined
-                methods =  Stream.of((PsiMethod) constructor);
-            } else if (constructor instanceof PsiClassImpl) {
-                // no constructor defined
-                final PsiClassImpl resolve = (PsiClassImpl) methodReference.resolve();
-                methods = resolve == null ? Stream.empty() : Arrays.stream(resolve.getConstructors());
-            } else {
-                throw new IllegalArgumentException("I don't know how to handle constructor " + constructor);
-            }
-            return methods;
+    @Contract(pure = true)
+    @NotNull
+    private static Stream<PsiMethod> methodsFromConstructors(@NotNull final PsiMethodReferenceExpressionImpl methodReference) {
+        final Stream<PsiMethod> methods;
+        final PsiElement constructor = methodReference.resolve();
+        if (constructor == null) {
+            // multiple constructors
+            final JavaResolveResult[] javaResolveResults = methodReference.multiResolve(true);
+            methods = Arrays.stream(javaResolveResults)
+                    .map(JavaResolveResult.class::cast)
+                    .map(JavaResolveResult::getElement)
+                    .map(PsiMethod.class::cast)
+            ;
+        } else if (constructor instanceof PsiMethod) {
+            // single constructor defined
+            methods =  Stream.of((PsiMethod) constructor);
+        } else if (constructor instanceof PsiClassImpl) {
+            // no constructor defined
+            final PsiClassImpl resolve = (PsiClassImpl) methodReference.resolve();
+            methods = resolve == null ? Stream.empty() : Arrays.stream(resolve.getConstructors());
+        } else {
+            throw new IllegalArgumentException("I don't know how to handle constructor " + constructor);
         }
+        return methods;
     }
 }
